94cdf96b9f9e3cf0d4433e13ef0b5aba415f8c81
CowboyAragorn <jacobman0313@gmail.com>
2022-03-19 15:45:56 -0400

    basic homepage html

--- src/homepage.js
+++ src/homepage.js
@@ -1,10 +1,41 @@
 export default function home() {
-    let welcome = document.createElement('div')
-    welcome.innerText("Welcome to Fast Eddie's")
+
+
+    let homepage = document.createElement('div')
+    let uiDirectory = document.createElement('div');
+        let menu = document.createElement('div');
+            menu.innerText = 'Menu';
+            uiDirectory.appendChild(menu);
+        let about = document.createElement('div');
+            about.innerText = 'About';
+            uiDirectory.appendChild(about);
+        let order = document.createElement('div');
+            order.innerText = 'Order';
+            uiDirectory.appendChild(order);
+
+            
+
+    let title = document.createElement('div');
+    let header = document.createElement('h1');
+        header.innerText = "Fast Eddie's"
+        title.appendChild(header);
+    let subHeader = document.createElement('h2');
+        subHeader.innerText = 'Home of the Fat Shake & Grease Fries';
+        title.appendChild(subHeader);
+
+    let copy = document.createElement('div');
+        let copyText = document.createElement('p');
+            copyText.innerText = 'People come to Fast Eddy\'s for the casual atmosphere, down home cooking, and simple minded employees. People stay because of the food poisoning. The city health inspector called us "worse than CiCi\'s", but here at Fast Eddy\'s - We Let the Customer Decide.' 
+            copy.append(copyText);
+        let slopBurgerImg = document.createElement('img');
+            slopBurgerImg.setAttribute('src', '/home/jacob/theOdinProject/restaurantPage/src/img/slopBurger.jpg')   
+        copy.append(slopBurgerImg);
+
+
+    document.body.append(homepage)
+    homepage.appendChild(uiDirectory);   
+    homepage.appendChild(title);
+    homepage.appendChild(copy);
     
-
-
-    content.appendChild(welcome);
-
 
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-//import home from './homepage'; 
+import home from './homepage'; 
 console.log('Is this thing on?');
 console.log('why is this not working');
 
 
-function home() {
-    //let content = document.getElementById('content');
-    let welcome = document.createElement('div');
-    welcome.innerHTML = 'Welcome to Fast Eddie';
-   document.body.appendChild(welcome);
-
-
-}
-
 home();
-//document.body.appendChild(home());
